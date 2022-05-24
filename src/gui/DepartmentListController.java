06d79fa5c58837eba5d3968ed3f877269c200c88
Arthur Cech Francisco <arthurcech@gmail.com>
2022-05-24 18:44:20 -0300

    feat: add department service with method to find all departments and add a method in department list controller to populate the table view

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,11 +15,15 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService departmentService;
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
+	private ObservableList<Department> observableListDepartment;
 
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
@@ -43,6 +50,19 @@ public class DepartmentListController implements Initializable {
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+	public void updateTableView() {
+		if (departmentService == null) {
+			throw new IllegalStateException("departmentService was null");
+		}
+		List<Department> departments = departmentService.findAll();
+		observableListDepartment = FXCollections.observableArrayList(departments);
+		tableViewDepartment.setItems(observableListDepartment);
+	}
+
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
 	}
 
 }
