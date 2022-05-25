07be85b910ba090f9577883923b5f71ecfd28166
Arthur Cech Francisco <arthurcech@gmail.com>
2022-05-25 17:48:00 -0300

    refactor: implemented operation to save seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -115,6 +117,25 @@ public class SellerFormController implements Initializable {
 
 		seller.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			validationException.addError("email", "Field can't be empty");
+		}
+		seller.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			validationException.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			seller.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			validationException.addError("baseSalary", "Field can't be empty");
+		}
+		seller.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		seller.setDepartment(comboBoxDepartment.getValue());
+
 		if (validationException.getErrors().size() > 0) {
 			throw validationException;
 		}
@@ -125,9 +146,10 @@ public class SellerFormController implements Initializable {
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 	}
 
 	@FXML
@@ -158,7 +180,6 @@ public class SellerFormController implements Initializable {
 		this.txtEmail.setText(seller.getEmail());
 		Locale.setDefault(Locale.US);
 		this.txtBaseSalary.setText(String.format("%.2f", seller.getBaseSalary()));
-
 		if (seller.getBirthDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(seller.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
