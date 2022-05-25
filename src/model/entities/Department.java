49909db9d278aa26e2eb7f55512b378c4a087374
Arthur Cech Francisco <arthurcech@gmail.com>
2022-05-25 08:33:21 -0300

    feat: add connection to mysql database and created auxiliar classes (DAO, connection, entities and exceptions)

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -35,25 +35,23 @@ public class Department implements Serializable {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Department that = (Department) o;
+		return id.equals(that.id);
+	}
+
+	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Department other = (Department) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	@Override
 	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
+		return "Department{" + "id=" + id + ", name='" + name + '\'' + '}';
 	}
 
 }
